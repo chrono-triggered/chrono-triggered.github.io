768df5af0d90da8d43bcf785bb2bbf13112e3937
chrono-triggered <michaelperez.perez123@gmail.com>
2019-03-29 15:29:50 -0500

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,32 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to my Website ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+}
+else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to my Website ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/images.png') {
+    myImage.setAttribute('src', 'images/zoso.png');
+  }
+  else {
+    myImage.setAttribute('src', 'images/images.png');
+  }
+}
