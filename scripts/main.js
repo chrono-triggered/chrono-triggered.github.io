fc04468d4ca73525ac7e32a9d861e9ed9824d0a7
Michael Perez <michaelperez.perez123@gmail.com>
2019-04-11 14:52:30 -0500

    Made links open in new tab, added url to image

--- scripts/main.js
+++ scripts/main.js
@@ -23,10 +23,13 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   var mySrc = myImage.getAttribute('src');
+  var myLink = myImage.getAttribute('class');
   if(mySrc === 'images/images.png') {
     myImage.setAttribute('src', 'images/zoso.png');
+    myLink.setAttribute('class', 'https://en.wikipedia.org/wiki/Led_Zeppelin_IV');
   }
   else {
     myImage.setAttribute('src', 'images/images.png');
+    myImage.setAttribute('class', 'https://www.ancient.eu/Yin_and_Yang/');
   }
 }
